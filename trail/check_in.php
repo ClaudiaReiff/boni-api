72a159c46b9ae3bfa376450f101c97385a16afa4
ClaudiaReiff <claudia.reiff@yoshie.io>
2023-10-19 16:37:50 +0200

    small fix

---
+++ trail/check_in.php
@@ -0,0 +1,8 @@
+<?php
+include '../connection.php';
+
+$id = $_POST['id'];
+$longitude = $_POST['longitude'];
+$latitude = $_POST['latitude'];
+$trailId = $_POST['trailId'];
+$checkedIn = $_POST['checkedIn'];
